66b6eef8b0e8a32ca856de4003d51f9a7a853ce0
ken-sayama <sayamaken0402@gmail.com>
2018-11-08 00:38:24 +0900

    add brew packages

--- install.sh
+++ install.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+# install homebrew
+eval '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+
+# homebrew install cli
+CLIBREWS=(
+forego
+git
+git-flow
+heroku
+rbenv
+ruby-build
+python
+redis
+postgresql
+)
+
+for CLIBREW in ${CLIBREWS[@]};
+do
+  brew install $CLIBREW -y
+done
+
+# homebrew cask install gui
+GUIBREWS=(
+iterm2
+google-chrome
+tower
+postgres
+sequel-pro
+imageoptim
+)
+
+for GUIBREW in ${GUIBREWS[@]};
+do
+  brew cask install $GUIBREW -y --appdir=~/Applications
+done
+

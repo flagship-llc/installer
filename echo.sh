d0dda9a6c14e6abeabc76c33989f66b163aa395c
ken-sayama <sayamaken0402@gmail.com>
2018-11-08 13:39:58 +0900

    rm echo.sh

--- echo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-LATEST_RUBY=$(rbenv install -l | grep -v - | tail -1)
-echo $LATEST_RUBY
--- test.sh
+++ test.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-while getopts e: OPT
-do
-  case $OPT in
-    e) pass=$OPTARG
-       ;;
-  esac
-done
-
-eval "echo $pass"
+echo hoge

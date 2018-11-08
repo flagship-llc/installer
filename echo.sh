cb72c8789930ec1cdd36250d811dec1a1d44073d
ken-sayama <sayamaken0402@gmail.com>
2018-11-08 15:26:11 +0900

    rm files

--- echo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#saveIFS=$IFS
-#IFS='=&'
-#parm=($QUERY_STRING)
-#IFS=$saveIFS
-
-#echo ${param[0]}
-echo $1
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while getopts e: OPT
-do
-  case $OPT in
-    e) pass=$OPTARG
-       ;;
-  esac
-done
-eval "echo $pass"

e258670749825488f0038acaeeca008fe6f79798
ken-sayama <sayamaken0402@gmail.com>
2018-11-19 17:14:57 +0900

    add pm-tool-install.sh

---
+++ pm-tool-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo ---------- Project Manager setup script start --------------
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+# install homebrew
+eval '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+
+TOOLS=(
+slack
+dropbox
+1password
+cloudapp
+sketch
+textexpander
+)
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo ----------- Project Manager setup script end --------------
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
